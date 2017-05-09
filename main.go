2c4f30e7c69d06fe07b02d114dd94091e47b0fb6
Romain De Oliveira <romainpaulo.deoliveira@gmail.com>
2017-05-09 20:46:27 +0200

    Showing Web back end in go

--- main.go
+++ main.go
@@ -1,28 +1,14 @@
 package main
 
-import "strconv"
+import (
+	"fmt"
+	"net/http"
+)
 
-const fizz = "FIZZ"
-
-const buzz = "BUZZ"
-
-//Compute the fizzbuzz value of an integer :
-// Fizz if %3=0, Buzz if %5=0, FizzBuzz if both
-func fizzbuzz(value int) string {
-	isfizz := value%3 == 0
-	isbuzz := value%5 == 0
-
-	if isfizz && isbuzz {
-		return fizz + buzz
-	}
-	if isfizz {
-		return fizz
-	}
-	if isbuzz {
-		return buzz
-	}
-	return strconv.Itoa(value)
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello World")
 }
-
 func main() {
+	http.HandleFunc("/", hello)
+	http.ListenAndServe(":8080", nil)
 }
